5a78b0f4a68a98cdf788ef5dd089bedfecdf76bf
lpopivnyak <136169348+lpopivnyak@users.noreply.github.com>
2023-12-02 19:28:00 +0200

    lesson1

---
+++ kahootik.py
@@ -0,0 +1,48 @@
+
+from PyQt5.QtWidgets import *
+
+app = QApplication([])
+
+window = QWidget()
+window.resize(500, 500)
+lbl = QLabel("Скільки буде 2+2?")
+ans1 = QRadioButton("4")
+ans2 = QRadioButton("14")
+ans3 = QRadioButton("30")
+ans4 = QRadioButton("900")
+
+main_line = QVBoxLayout()
+main_line.addWidget(lbl)
+
+h1 = QHBoxLayout()
+h1.addWidget(ans1)
+h1.addWidget(ans2)
+main_line.addLayout(h1)
+
+h2 = QHBoxLayout()
+h2.addWidget(ans3)
+h2.addWidget(ans4)
+main_line.addLayout(h2)
+
+
+def true_var_1():
+    msg = QMessageBox()
+    msg.setText("Правильно")
+    msg.exec()
+def false_var_1():
+    msg = QMessageBox()
+    msg.setText("Не Правильно")
+    msg.exec()
+
+
+ans1.clicked.connect(true_var_1)
+ans2.clicked.connect(false_var_1)
+ans3.clicked.connect(false_var_1)
+ans4.clicked.connect(false_var_1)
+
+
+
+window.setLayout(main_line)
+window.show()
+app.exec()
+
